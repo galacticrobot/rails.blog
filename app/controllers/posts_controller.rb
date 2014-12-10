95db76e8dc5588eee61ba802944e63cd2f535a9f
Sebastian Scarpa <sebastian.scarpa@gmail.com>
2014-12-10 21:22:26 +0100

    added postlisting on index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,24 @@
 class PostsController < ApplicationController
 
     def index
-        @post = Post.all.order('created_at DESC')
+        @posts = Post.all.order('created_at DESC') #Get all posts and order them desc by creatin date
     end
 
     def new
     end
 
     def create
-        @post = Post.new(post_params)
+        @post = Post.new(post_params) #defined below in private
         @post.save
         redirect_to @post #what to do when action is finished.
     end
 
     def show
-        @post = Post.find(params[:id])
+        @post = Post.find(params[:id]) #find posts by ID.
     end
 
     private
-        # strong parameters - what is allowed to be saved
+        # strong parameters - what is allowed to be saved. In this case title and body.
         def post_params
             params.require(:post).permit(:title, :body)
         end
