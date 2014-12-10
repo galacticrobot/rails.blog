c19f84be3caeb9745efdd495a3f398f59142d622
Sebastian Scarpa <sebastian.scarpa@gmail.com>
2014-12-10 21:01:01 +0100

    added create and show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,27 @@
 class PostsController < ApplicationController
 
     def index
+        @post = Post.all.order('created_at DESC')
     end
 
     def new
     end
+
+    def create
+        @post = Post.new(post_params)
+        @post.save
+        redirect_to @post #what to do when action is finished.
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    private
+        # strong parameters - what is allowed to be saved
+        def post_params
+            params.require(:post).permit(:title, :body)
+        end
+
 
 end
