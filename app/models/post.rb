2308b5dbd2ac9bb0ead281b8482d34db9ec55930
Sebastian Scarpa <sebastian.scarpa@gmail.com>
2014-12-13 00:00:14 +0100

    added comments and commentscontroller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-    has_many :comments #asociate posts with comments
+    has_many :comments, dependent: :destroy#asociate posts with comments
 
     #makes sure there is at least 5 char in title and content in title and body
     validates :title, presence: true, length: { minimum: 5 }
