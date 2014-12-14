bc2088e7db12984bb92fe6fcd1af14075fdd9efe
Sebastian Scarpa <sebastian.scarpa@gmail.com>
2014-12-14 16:30:43 +0100

    added devise and admin user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    #devise settings
+    before_action :authenticate_user!, except: [:index, :show]
 
     def index
         @posts = Post.all.order('created_at DESC') #Get all posts and order them desc by creatin date
